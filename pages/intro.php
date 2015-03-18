9f3984dc2267f39f0f6dfc14df176c280a7f72dd
Tom Peerdeman <tom@tompeerdeman.nl>
2015-03-18 18:46:25 +0100

    Initial Twitter test of David

--- pages/intro.php
+++ pages/intro.php
@@ -1,1 +1,33 @@
 This is the intro page. Choose a job cluster from the left menu.
+<?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'On');
+
+    require_once("libraries/TwitterAPIExchange.php")
+    $settings = array(
+                      'oauth_access_token' => "3017875300-T4AFO9OC5J5m0zr8JTF9bvsSdH8wxCvSMFgXiKI",
+                      'oauth_access_token_secret' => "zJx3lmIAw9jt1GTsSTEBuAW9da6HcBAQnSD8vlZQEzDaQ",
+                      'consumer_key' => "ofOVAB4toaGIFJv9IrBcIRUIl",
+                      'consumer_secret' => "asoNqY7cqVj9heosApHJdgELsHEAyvzDuy4dqRLOcJ8OyGarjK"
+                      );
+    
+    $url = "https://api.twitter.com/1.1/search/tweets.json";
+    
+    $requestMethod = "GET";
+    
+    $getfield = '?q=&geocode=-22.912214,-43.230182,1km&lang=pt&result_type=recent';
+    
+    $twitter = new TwitterAPIExchange($settings);
+    $string = json_decode($twitter->setGetfield($getfield)
+                          ->buildOauth($url, $requestMethod)
+                          ->performRequest(),$assoc = TRUE);
+    if($string["errors"][0]["message"] != "") {
+		echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";
+		exit();
+	}
+
+    echo "<pre>";
+    print_r($string);
+    echo "</pre>";
+    
+?>
