7050f5860950683dc960b2d05def44bbb49ba103
Tom Peerdeman <tom@tompeerdeman.nl>
2015-03-18 16:06:46 +0100

    Working RDF insert from PHP

--- inserttest.php
+++ inserttest.php
@@ -6,7 +6,6 @@ require_once("../libraries/EasyRdf.php");
 
 use \EasyRdf_Graph as Graph;
 use \EasyRdf_GraphStore as GraphStore;
-use \EasyRdf_Sparql_Client as Sparql_Client;
 
 ?>
 
@@ -19,20 +18,17 @@ use \EasyRdf_Sparql_Client as Sparql_Client;
 <body>
 <pre>
 <?php
-
 // Use a local SPARQL 1.1 Graph Store (eg RedStore)
-//$gs = new GraphStore('http://iwa2.cf:8080/openrdf-sesame/repositories/iwa');
-$client = new Sparql_Client('http://iwa2.cf:8080/openrdf-sesame/repositories/iwa', 'http://iwa2.cf:8080/openrdf-sesame/repositories/iwa/statements');
-
+$gs = new GraphStore('http://iwa2.cf:8080/openrdf-sesame/repositories/iwa/statements');
 // Add the current time in a graph
 $graph1 = new Graph();
 $graph1->add('http://example.com/test', 'rdfs:label', 'Test');
 $graph1->add('http://example.com/test', 'dc:date', time());
-$client->insert($graph1);
-//$gs->insertIntoDefault($graph1);
+
+$gs->insertIntoDefault($graph1, 'rdfxml');
 // Get the graph back out of the graph store and display it
-//$graph2 = $gs->getDefault();
-//print $graph2->dump();
+$graph2 = $gs->getDefault();
+print $graph2->dump();
 ?>
 </pre>
 </body>
