e9fc9d3a33b63ea2067a351542339d5ba47cea08
Tom Peerdeman <tom@tompeerdeman.nl>
2015-03-20 00:00:54 +0100

    Fetch genders using bnode

---
+++ fetch/fetch_profile.php
@@ -0,0 +1,36 @@
+<pre>
+<?php
+if(!isset($_GET['occ'])) {
+	die("No occupation set");
+}
+$occ = $_GET['occ'];
+
+require_once("fetch_core.php");
+
+$sparql = <<<SPARQL
+PREFIX dbprop: <http://dbpedia.org/property/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+PREFIX dcterms: <http://purl.org/dc/terms/>
+PREFIX iwa: <http://iwa2.cf/>
+
+CONSTRUCT {
+	<{$occ}> iwa:genderData [iwa:gender ?gender ;
+		iwa:count ?genderCount ]. 
+} WHERE {
+	{
+		SELECT COUNT(*) AS ?genderCount ?gender
+		WHERE {
+			?person dbpedia-owl:occupation <{$occ}> .
+			?person dbpprop:gender ?gender .
+		}
+		GROUP BY ?gender
+	}
+}
+SPARQL;
+
+$graph = $sparqlClient->query($sparql);
+var_dump($graphStore->insertIntoDefault($graph, 'rdfxml'));
+
+?>
+</pre>
