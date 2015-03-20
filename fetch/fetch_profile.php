345c8673e0edd6d8e9be468bc232955ba30e609c
Tom Peerdeman <tom@tompeerdeman.nl>
2015-03-20 01:38:48 +0100

    Fetch birthDate

--- fetch/fetch_profile.php
+++ fetch/fetch_profile.php
@@ -60,5 +60,33 @@ $graph = $sparqlClient->query($sparql);
 
 var_dump($graphStore->insertIntoDefault($graph, 'rdfxml'));
 
+
+/* insert age */
+$sparql = <<<SPARQL
+PREFIX dbprop: <http://dbpedia.org/property/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+PREFIX dcterms: <http://purl.org/dc/terms/>
+PREFIX iwa: <http://iwa2.cf/>
+
+CONSTRUCT {
+	<{$occ}> iwa:ageData [iwa:birthDate ?date ;
+		iwa:count ?dateCount ].
+} WHERE {
+	{
+		SELECT COUNT(*) AS ?dateCount ?date
+		WHERE {
+			?person dbpedia-owl:occupation <{$occ}> .
+			?person dbpedia-owl:birthDate ?date .
+		}
+		GROUP BY ?date
+	}
+}
+SPARQL;
+
+$graph = $sparqlClient->query($sparql);
+
+var_dump($graphStore->insertIntoDefault($graph, 'rdfxml'));
+
 ?>
 </pre>
