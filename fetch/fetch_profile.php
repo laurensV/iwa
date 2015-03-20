7a76b06e2496a8ff1b81d982f966605c944a4868
Laurens Verspeek <laurens_verspeek@hotmail.com>
2015-03-20 01:23:26 +0100

    Fetch religion

--- fetch/fetch_profile.php
+++ fetch/fetch_profile.php
@@ -16,7 +16,7 @@ PREFIX iwa: <http://iwa2.cf/>
 
 CONSTRUCT {
 	<{$occ}> iwa:genderData [iwa:gender ?gender ;
-		iwa:count ?genderCount ]. 
+		iwa:count ?genderCount ].
 } WHERE {
 	{
 		SELECT COUNT(*) AS ?genderCount ?gender
@@ -30,6 +30,34 @@ CONSTRUCT {
 SPARQL;
 
 $graph = $sparqlClient->query($sparql);
+
+var_dump($graphStore->insertIntoDefault($graph, 'rdfxml'));
+
+/* insert religion */
+$sparql = <<<SPARQL
+PREFIX dbprop: <http://dbpedia.org/property/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+PREFIX dcterms: <http://purl.org/dc/terms/>
+PREFIX iwa: <http://iwa2.cf/>
+
+CONSTRUCT {
+	<{$occ}> iwa:religionData [iwa:religion ?religion ;
+		iwa:count ?religionCount ].
+} WHERE {
+	{
+		SELECT COUNT(*) AS ?religionCount ?religion
+		WHERE {
+			?person dbpedia-owl:occupation <{$occ}> .
+			?person dbpprop:religion ?religion .
+		}
+		GROUP BY ?religion
+	}
+}
+SPARQL;
+
+$graph = $sparqlClient->query($sparql);
+
 var_dump($graphStore->insertIntoDefault($graph, 'rdfxml'));
 
 ?>
